66c378b72485403d575e936c47dee2d50c46f943
Yael Montufar <yael.montufar@gmail.com>
2020-07-05 22:29:53 -0500

    can create a new page with title; page model validates presence & uniqueness; title parameter is downcased before saving to database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,26 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
   end
 
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to root_path
+    else
+    render :new
+    end
+  end
+
   def kitchen_sink
   end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title)
+  end
+
 end
