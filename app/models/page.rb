c35500443b83c8077bb38f01d204a115eddb6356
Yael Montufar <yael.montufar@gmail.com>
2020-07-06 22:13:47 -0500

    changed title format before validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,15 +1,19 @@
 class Page < ApplicationRecord
+  before_validation :title_format
   after_validation :set_slug, :to_param, only: [:show]
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }
-
-  before_save { title.downcase! }
 
   def to_param
     slug.to_s
   end
 
   private
+
+  def title_format
+    # self.title = title.gsub(/\s+/, "")
+    title.downcase!
+  end
 
   def set_slug
     self.slug = title.parameterize
