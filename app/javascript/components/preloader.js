88a2a92a92d15a40d9f9eef6f29a6fe74fdef6e2
Yael Montufar <yael.montufar@gmail.com>
2020-07-10 20:20:09 -0500

    fixed indentation and added some commentary in preloader function

--- app/javascript/components/preloader.js
+++ app/javascript/components/preloader.js
@@ -3,7 +3,8 @@ const loadSvg = () => {
     setTimeout(() => {
       const preload = document.querySelector('.preload');
       if (preload !== null) {
-      preload.classList.add('postload');
+        // prevents console error message when proload class is not found onother pages
+        preload.classList.add('postload');
       }
     }, 3100); // 3600ms (animation) - .5s (preload)
   })
